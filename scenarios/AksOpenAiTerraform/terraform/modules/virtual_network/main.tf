ff91a32ad10bacfc585f06bb4f4f4c1fc87e9085
Aria Amini <ariaamini@microsoft.com>
2024-11-27 10:17:55 -0500

    Remove log

--- scenarios/AksOpenAiTerraform/terraform/modules/virtual_network/main.tf
+++ scenarios/AksOpenAiTerraform/terraform/modules/virtual_network/main.tf
@@ -40,10 +40,6 @@ resource "azurerm_monitor_diagnostic_setting" "settings" {
   target_resource_id         = azurerm_virtual_network.vnet.id
   log_analytics_workspace_id = var.log_analytics_workspace_id
 
-  enabled_log {
-    category = "VMProtectionAlerts"
-  }
-
   metric {
     category = "AllMetrics"
   }
