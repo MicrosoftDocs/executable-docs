995ec7a1618fbecfae441731c47610637d5c2824
Aria Amini <ariaamini@microsoft.com>
2024-11-27 02:00:23 -0500

    Another fix

--- scenarios/AksOpenAiTerraform/terraform/modules/storage_account/main.tf
+++ scenarios/AksOpenAiTerraform/terraform/modules/storage_account/main.tf
@@ -2,13 +2,14 @@ resource "azurerm_storage_account" "storage_account" {
   name                = var.name
   resource_group_name = var.resource_group_name
 
-  public_network_access_enabled = false
   location                 = var.location
   account_kind             = var.account_kind
   account_tier             = var.account_tier
   account_replication_type = var.replication_type
   is_hns_enabled           = var.is_hns_enabled
   tags                     = var.tags
+
+  allow_nested_items_to_be_public = false
 
   network_rules {
     default_action             = (length(var.ip_rules) + length(var.virtual_network_subnet_ids)) > 0 ? "Deny" : var.default_action
