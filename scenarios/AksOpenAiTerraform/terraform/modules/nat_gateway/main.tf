fcf07ed77421808a1777f4631095e8d3c64e6660
Aria Amini <ariaamini@microsoft.com>
2025-02-24 20:16:19 -0500

    Rename

--- scenarios/AksOpenAiTerraform/terraform/modules/nat_gateway/main.tf
+++ scenarios/AksOpenAiTerraform/terraform/modules/nat_gateway/main.tf
@@ -1,13 +1,7 @@
-locals {
-  zones = ["1"]
-}
-
 resource "azurerm_nat_gateway" "this" {
   name                    = var.name
   location                = var.location
   resource_group_name     = var.resource_group_name
-  idle_timeout_in_minutes = 4
-  zones                   = local.zones
 }
 
 resource "azurerm_public_ip" "nat_gateway" {
@@ -15,7 +9,6 @@ resource "azurerm_public_ip" "nat_gateway" {
   location            = var.location
   resource_group_name = var.resource_group_name
   allocation_method   = "Static"
-  zones               = local.zones
 }
 
 resource "azurerm_nat_gateway_public_ip_association" "nat_gategay_public_ip_association" {
@@ -23,7 +16,7 @@ resource "azurerm_nat_gateway_public_ip_association" "nat_gategay_public_ip_asso
   public_ip_address_id = azurerm_public_ip.nat_gateway.id
 }
 
-resource "azurerm_subnet_nat_gateway_association" "nat-avd-sessionhosts" {
+resource "azurerm_subnet_nat_gateway_association" "gateway_association" {
   for_each       = var.subnet_ids
   subnet_id      = each.value
   nat_gateway_id = azurerm_nat_gateway.this.id
