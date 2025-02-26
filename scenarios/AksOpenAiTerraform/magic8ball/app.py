d34bd3a98686ff1c21cd8ed9d5a52cc5e1c88eaa
Aria Amini <ariaamini@microsoft.com>
2025-02-26 06:17:18 -0500

    ui tweaks

--- scenarios/AksOpenAiTerraform/magic8ball/app.py
+++ scenarios/AksOpenAiTerraform/magic8ball/app.py
@@ -2,50 +2,25 @@
 #
 # Make sure to provide a value for the following environment variables:
 #  - AZURE_OPENAI_BASE (ex: https://eastus.api.cognitive.microsoft.com/)
-#  - AZURE_OPENAI_KEY
 #  - AZURE_OPENAI_DEPLOYMENT
 #  - AZURE_OPENAI_MODEL
-#  - TITLE
-#  - TEMPERATURE
-#  - SYSTEM (Used to describe the assistant's personality.)
-#
-# You can use two different authentication methods:
-# 
-#  - API key: set the AZURE_OPENAI_TYPE environment variable to azure and the AZURE_OPENAI_KEY environment variable to the key of  
-#   your Azure OpenAI resource. You can use the regional endpoint, such as https://eastus.api.cognitive.microsoft.com/, passed in 
-#   the AZURE_OPENAI_BASE environment variable, to connect to the Azure OpenAI resource.
-#
-#  - Azure Active Directory: set the AZURE_OPENAI_TYPE environment variable to azure_ad and use a service principal or managed 
-#   identity with the DefaultAzureCredential object to acquire a token. For more information on the DefaultAzureCredential in Python, 
-#   see https://docs.microsoft.com/en-us/azure/developer/python/azure-sdk-authenticate?tabs=cmd
-#   Make sure to assign the "Cognitive Services User" role to the service principal or managed identity used to authenticate to 
-#   Azure OpenAI. For more information, see https://learn.microsoft.com/en-us/azure/cognitive-services/openai/how-to/managed-identity.
-#   If you want to use Azure AD integrated security, you need to create a custom subdomain for your Azure OpenAI resource and use the 
-#   specific endpoint containing the custom domain, such as https://bingo.openai.azure.com/ where bingo is the custom subdomain. 
-#   If you specify the regional endpoint, you get a wonderful error: "Subdomain does not map to a resource.". 
-#   Hence, make sure to pass the endpoint containing the custom domain in the AZURE_OPENAI_BASE environment variable.
-#
-# Use the following command to run the app: 
-# - streamlit run app.py
+#  - AZURE_OPENAI_VERSION
 
 import os
-import sys
 import time
 import openai
 import logging
 import streamlit as st
 from streamlit_chat import message
 from azure.identity import DefaultAzureCredential
-from dotenv import load_dotenv
-from dotenv import dotenv_values
-
-# Load environment variables from .env file
-if os.path.exists(".env"):
-  load_dotenv(override=True)
-  config = dotenv_values(".env")
 
 # Read environment variables
-assistan_profile = """
+api_base = os.getenv("AZURE_OPENAI_BASE")
+api_version = os.environ.get("AZURE_OPENAI_VERSION")
+engine = os.getenv("AZURE_OPENAI_DEPLOYMENT")
+model = os.getenv("AZURE_OPENAI_MODEL")
+
+system = """
 You are the infamous Magic 8 Ball. You need to randomly reply to any question with one of the following answers:
 
 - It is certain.
@@ -72,58 +47,21 @@ You are the infamous Magic 8 Ball. You need to randomly reply to any question wi
 Add a short comment in a pirate style at the end! Follow your heart and be creative! 
 For mor information, see https://en.wikipedia.org/wiki/Magic_8_Ball
 """
-title = os.environ.get("TITLE", "Magic 8 Ball")
-text_input_label = os.environ.get("TEXT_INPUT_LABEL", "Pose your question and cross your fingers!")
-image_file_name = os.environ.get("IMAGE_FILE_NAME", "magic8ball.png")
-image_width = int(os.environ.get("IMAGE_WIDTH", 80))
-temperature = float(os.environ.get("TEMPERATURE", 0.9))
-system = os.environ.get("SYSTEM", assistan_profile)
-api_base = os.getenv("AZURE_OPENAI_BASE")
-api_key = os.getenv("AZURE_OPENAI_KEY")
-api_type = os.environ.get("AZURE_OPENAI_TYPE", "azure")
-api_version = os.environ.get("AZURE_OPENAI_VERSION", "2023-05-15")
-engine = os.getenv("AZURE_OPENAI_DEPLOYMENT")
-model = os.getenv("AZURE_OPENAI_MODEL")
+title = "Magic 8 Ball"
+text_input_label = "Pose your question and cross your fingers!"
+image_file_name = "magic8ball.png"
+image_width = 80
+temperature = 0.9
 
 # Configure OpenAI
-openai.api_type = api_type
+openai.api_type = "azure"
 openai.api_version = api_version
 openai.api_base = api_base 
 
-# Set default Azure credential
-default_credential = DefaultAzureCredential() if openai.api_type == "azure_ad" else None
-
-# Configure a logger
-logging.basicConfig(stream = sys.stdout, 
-                    format = '[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s',
-                    level = logging.INFO)
-logger = logging.getLogger(__name__)
-
-# Log variables
-logger.info(f"title: {title}")
-logger.info(f"text_input_label: {text_input_label}")
-logger.info(f"image_file_name: {image_file_name}")
-logger.info(f"image_width: {image_width}")
-logger.info(f"temperature: {temperature}")
-logger.info(f"system: {system}")
-logger.info(f"api_base: {api_base}")
-logger.info(f"api_key: {api_key}")
-logger.info(f"api_type: {api_type}")
-logger.info(f"api_version: {api_version}")
-logger.info(f"engine: {engine}")
-logger.info(f"model: {model}")
-
 # Authenticate to Azure OpenAI
-if openai.api_type == "azure":
-  openai.api_key = api_key
-elif openai.api_type == "azure_ad":
-  openai_token = default_credential.get_token("https://cognitiveservices.azure.com/.default")
-  openai.api_key = openai_token.token
-  if 'openai_token' not in st.session_state:
-    st.session_state['openai_token'] = openai_token
-else:
-  logger.error("Invalid API type. Please set the AZURE_OPENAI_TYPE environment variable to azure or azure_ad.")
-  raise ValueError("Invalid API type. Please set the AZURE_OPENAI_TYPE environment variable to azure or azure_ad.")
+default_credential = DefaultAzureCredential()
+openai_token = default_credential.get_token("https://cognitiveservices.azure.com/.default")
+openai.api_key = openai_token.token
 
 # Customize Streamlit UI using CSS
 st.markdown("""
@@ -299,12 +237,6 @@ with col5:
 # - normal: display the chat history as a list of messages using the streamlit_chat message() function 
 # - rich: display the chat history as a list of messages using the Streamlit markdown() function
 if st.session_state['generated']:
-  tab1, tab2 = st.tabs(["normal", "rich"])
-  with tab1:
-    for i in range(len(st.session_state['generated']) - 1, -1, -1):
-      message(st.session_state['past'][i], is_user = True, key = str(i) + '_user', avatar_style = "fun-emoji", seed = "Nala")
-      message(st.session_state['generated'][i], key = str(i), avatar_style = "bottts", seed = "Fluffy")
-  with tab2:
-    for i in range(len(st.session_state['generated']) - 1, -1, -1):
-      st.markdown(st.session_state['past'][i])
-      st.markdown(st.session_state['generated'][i])
+  for i in range(len(st.session_state['generated']) - 1, -1, -1):
+    message(st.session_state['past'][i], is_user = True, key = str(i) + '_user', avatar_style = "fun-emoji", seed = "Nala")
+    message(st.session_state['generated'][i], key = str(i), avatar_style = "bottts", seed = "Fluffy")
