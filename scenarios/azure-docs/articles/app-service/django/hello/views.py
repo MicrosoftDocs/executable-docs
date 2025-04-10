01028ff233611b7150519a828f70f9108e074807
naman-msft <naman.parikh@microsoft.com>
2025-04-10 13:33:15 -0700

    removed inactive metadata

--- scenarios/azure-docs/articles/app-service/django/hello/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def hello(request):
-    return HttpResponse("Hello, World!")
--- scenarios/azure-docs/articles/app-service/flask/app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    print("Handling request to home page.")
-    return "Hello, Azure!"
